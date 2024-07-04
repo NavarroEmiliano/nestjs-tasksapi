89a478017b0a9aa121d222cd71803aeab2a74da3
Emiliano Navarro <emilianonavarro1993@gmail.com>
2024-07-03 23:27:46 -0300

    tasks service

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Task } from 'src/schemas/tasks.schema';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  constructor(@InjectModel(Task.name) private taskModel: Model<Task>) {}
+
+  findAll() {
+    this.taskModel.find();
+  }
+
+  async create(createTask: any) {
+    const newTask = new this.taskModel(createTask);
+    await newTask.save();
+    return newTask;
+  }
+
+  async findOne(id: string) {
+    return this.taskModel.findById(id);
+  }
+  async delete(id: string) {
+    return this.taskModel.findByIdAndDelete(id);
+  }
+
+  async update(id: string, task: any) {
+    return this.taskModel.findByIdAndUpdate(id, task);
+  }
+}
