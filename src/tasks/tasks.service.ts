787623224e53db8319db1d03fa7c378d9a122038
Emiliano Navarro <emilianonavarro1993@gmail.com>
2024-07-04 11:31:01 -0300

    dtos

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Task } from 'src/schemas/tasks.schema';
-
+import { CreateTaskDto } from '../dto/create-task.dto';
+import { UpdateTaskDto } from '../dto/update-task.dto';
 @Injectable()
 export class TasksService {
   constructor(@InjectModel(Task.name) private taskModel: Model<Task>) {}
@@ -11,7 +12,7 @@ export class TasksService {
     this.taskModel.find();
   }
 
-  async create(createTask: any) {
+  async create(createTask: CreateTaskDto) {
     const newTask = new this.taskModel(createTask);
     await newTask.save();
     return newTask;
@@ -24,7 +25,7 @@ export class TasksService {
     return this.taskModel.findByIdAndDelete(id);
   }
 
-  async update(id: string, task: any) {
+  async update(id: string, task: UpdateTaskDto) {
     return this.taskModel.findByIdAndUpdate(id, task);
   }
 }
